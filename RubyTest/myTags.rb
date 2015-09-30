d49146ae289d029ca54d025fad1b4125b0b53eaa
Andrew Dinh <andrewdinh74@yahoo.com>
2015-09-29 23:09:02 -0700

    Add the findKey method to myDict class

--- RubyTest/myTags.rb
+++ RubyTest/myTags.rb
@@ -1,3 +1,15 @@
+require 'set'
+
 class myDict
   def initialize
-    @tagDict = {'NAME' => ['jack', 'jill'], 'NUM' => ['one', 'two', 'three', 'four', 'nine']}
+    @tagDict = {'NAME' => ['jack', 'jill'].to_set, 'NUM' => ['one', 'two', 'three', 'four', 'nine'].to_set}
+  end
+
+  def findKey(term)
+    @tagDict.each do |key, value|
+      if value.include? term
+        return key
+      else
+        next
+    end
+  end
