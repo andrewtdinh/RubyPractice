0e6a2dc800ee0b0b495fa011b2bd5aba347dd689
Andrew Dinh <andrewdinh74@yahoo.com>
2015-09-30 19:15:24 -0700

    Add an initializer to myTagProcessor class

--- RubyTest/myTagProcessor.rb
+++ RubyTest/myTagProcessor.rb
@@ -1,3 +1,12 @@
 require 'myTags'
+require 'set'
 
 class Taggerizer
+  def initialize(inputString)
+    @originalStr = inputString
+    # The next two instance variables are sets because we want unique values inside
+    @taggedTerms = Set.new
+    @untaggedTerms = Set.new
+    # The next variable is an array because we want the terms to be in the order we add them
+    @replacedStr = []
+  end
